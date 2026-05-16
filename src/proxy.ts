21baf0e361b688e86fba019b46740ca79fdd50f3
M-Vishal-11 <m.vishal9a@gmail.com>
2026-05-16 15:33:49 +0530

    proxy update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/auth";
 
-export const proxy = auth((req) => {});
+export default auth((req) => {});
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
